96181817cff991c117f7df8c4b83399cd3153b27
Connor Ladly-Fredeen <connorladlyfredeen@gmail.com>
2025-01-16 16:35:20 -0500

    chore: update initial copy

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 [![Netlify Status](https://api.netlify.com/api/v1/badges/744ddbe1-43ef-4e32-847c-3f383006f9a0/deploy-status)](https://app.netlify.com/sites/connorladly-com/deploys)
+
 In progress
--- pages/about.vue
+++ pages/about.vue
@@ -2,8 +2,21 @@
   <div class="prose mx-auto py-20">
     <h1>About Connor</h1>
     <p>
-      I'm Connor Ladly-Fredeen, a director of engineering with a passion for scaling high-throughput systems, running,
-      and volunteering. Explore more about my background, skills, and hobbies here.
+      I'm Connor Ladly-Fredeen, a director of engineering with a passion 
+      for scaling high-throughput systems, building happy and performant 
+      teams, and developing awesome products.
+    </p>
+    <p>
+      I've been working in tech for over 10 years, and I've had the 
+      opportunity to work with some amazing people and companies. I've 
+      worked on everything from small startups to large enterprises, and 
+      I've learned a lot along the way.
+    </p>
+    <p>
+      Pardon the lack of styling, this site is perpetually in progress and
+      a chance to play around with new techbnologies. As of Jan 2025 it is a Nuxt.j
+      site hosted on Netlify, with content stored in a GitHub repo and proxied through
+      a GCP Instance.
     </p>
   </div>
 </template>
